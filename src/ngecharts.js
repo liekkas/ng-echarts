27afe45fe87b1f8e1ecbbf37808097e62cb8b79a
liekkas.zeng <zengjiangqin@gmail.com>
2015-01-10 17:50:47 +0800

    use isolated scope

--- src/ngecharts.js
+++ src/ngecharts.js
@@ -8,31 +8,32 @@ angular.module('ng-echarts',['ng-echarts.theme'])
                 var chart = echarts.init(element[0]);
 
                 function refreshChart(){
-
-                    var option = scope[attrs['option']];
-                    var tn = theme.getTheme(scope[attrs['theme']]);
+                    var tn = theme.getTheme(scope.theme);
                     chart.clear();
-                    chart.setOption(option);
+                    chart.setOption(scope.option);
                     chart.setTheme(tn||{});
                     chart.resize();
                 };
 
-                //如果第一个参数直接写成scope[attrs['theme']];那么只会是第一个图表生效
+                //如果第一个参数直接写成scope.theme;那么只会是第一个图表生效
                 //问题可见 http://segmentfault.com/q/1010000000720618
                 scope.$watch(
-                    function () { return scope[attrs['theme']]; },
+                    function () { return scope.theme; },
                     function (value) {if (value) {refreshChart();}}
                 );
 
                 //第三个参数需要设置为true，只要option上某个属性变化就能刷新图表
                 //很显然咱们需要做深度侦察
                 scope.$watch(
-                    function () { return scope[attrs['option']]; },
+                    function () { return scope.option; },
                     function (value) {if (value) {refreshChart();}},
                     true
                 );
             },
-            scope:true,
+            scope:{
+                theme:'=',
+                option:'='
+            },
             restrict:'EA'
         }
     }]);
